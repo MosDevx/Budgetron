47ed2b225dd5fcc2d6a5fcd37890fecb1daec44c
MosDevx <mosesk.wamae@gmail.com>
2023-06-01 23:46:50 +0300

    apply migrations

--- db/migrate/20230601202757_add_columns_to_financial_transactions.rb
+++ db/migrate/20230601202757_add_columns_to_financial_transactions.rb
@@ -1,6 +1,8 @@
 class AddColumnsToFinancialTransactions < ActiveRecord::Migration[7.0]
   def change
-    add_column :financial_transactions, :author_id, :string ,default: -> { SecureRandom.uuid }, null: false
+      enable_extension 'uuid-ossp'
+      add_column :financial_transactions, :author_id, :uuid, default: -> { "uuid_generate_v4()" }, null: false
+    
     add_column :financial_transactions, :name, :string
     add_column :financial_transactions, :amount, :decimal
   end
