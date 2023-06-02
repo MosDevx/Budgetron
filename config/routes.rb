a016be2cb84d958bf3d7c0a8fe727574aace1fdc
MosDevx <mosesk.wamae@gmail.com>
2023-06-02 06:30:07 +0300

    set up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :categories
   resources :financial_transaction_categories
   resources :financial_transactions
@@ -6,5 +7,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "categories#index"
 end
